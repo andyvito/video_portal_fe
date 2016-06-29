39c0eee07edd184c490ab1f9ba32e5ae616b5d63
vagrant <allan.duarte@gmail.com>
2016-06-29 23:47:01 +0000

    integrate services login and logout

--- client/appAngular.js
+++ client/appAngular.js
@@ -1,32 +1,51 @@
 (function(){
-	var app = angular.module('videoPortal',['ui.router']);
+"use strict";
+
+	var app = angular.module('videoPortal',['ui.router','ngCookies','angular-md5']);
 
 
+	app.controller('LoginController', ['$scope','$rootScope','$location', '$cookies', 'loginService', 'md5', 
+		function($scope,$rootScope, $location, $cookies, loginService, md5){
 
+			$scope.submit = function(){				
+				$scope.user.password = md5.createHash($scope.user.password || '');
+				loginService.login($scope.user)
+						.then(function(data){
+					    		if (data.status && data.sessionId){
+					    			$cookies.put('sessionId', data.sessionId);
+					    			$rootScope.$apply(function(){
+					    				$location.path('videos');	
+					    			});		    	
+					    		}
+					    		else{
+					    			//TODO: clean password and username from UI
+									$location.path('login');
+									console.log('no found');
+									console.log(data);
+					    		}
+					    	});
+			};
 
-	app.controller('LoginController', function($scope,$location,$rootScope){
+	}]);
 
-		$scope.submit = function(){
-			var uname = $scope.username;
-			var password = $scope.password;
-			//Call the login service. If true,
-				$rootScope.loggedIn = true;
-				$location.path('videos');
-
+	app.controller('VideosController', ['$scope', '$location', '$cookies', 'loginService', 
+		function($scope,$location,$cookies,loginService){
+		$scope.logout = function($scope){
+			//$cookies.remove('sessionId');
+			loginService.logout($scope)
+					.then(function(data){
+						console.log(data);
+				        $location.path('/login');
+					});
 		};
-	});
 
-
-	app.controller('VideosController', function($scope,$location){
-
-		console.log('hola');
-	});
+	}]);
 
 
 	app.config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
 	  $stateProvider.state('home', {
-	      url: '/login',
+	      url: '/',
 	      templateUrl: '/login.html',
 	    })
 	    .state('login', {
@@ -37,12 +56,11 @@
 	  	.state('videos', {
 	  	  url: '/videos',
 	  	  resolve:{
-	  	  	function($location, $rootScope){
-	  	  		console.log($rootScope.loggedIn);
-	  	  		//TODO: check the user is logging
-	  	  		if (!$rootScope.loggedIn){
+	  	  	function($location, $cookies){
+	  	  		//$cookies.remove('sessionId')
+	  	  		console.log($cookies.get('sessionId'));
+	  	  		if (!$cookies.get('sessionId')){
 	  	  			$location.path('login');
-	  	  			console.log('entra');
 	  	  		}
 	  	  	},
 	  	  },
@@ -58,6 +76,42 @@
 
 	  $urlRouterProvider.otherwise('home');
 	}]);
+
+
+	app.factory('loginService', function($http, $location, $rootScope, $cookies, md5, ApiService){
+		return{
+		    login:function(user){				
+				var data = {username: user.username,
+							password: user.password};
+
+		    	return new Promise(function(resolve){
+		    		$http.post('/user/auth', data)
+		    				.success(function(data, status, headers){
+		    					user.username = "";
+		    					user.password = "";
+		    					resolve(data);
+		    				})
+		    				.error(function(data){
+		    					alert('Error');
+		    					console.log(data);
+		    				});
+    			});
+		    },
+		    logout:function($scope){				
+		    	return new Promise(function(resolve){
+		    		ApiService.logout($cookies.get('sessionId')).then(function(data){
+		    			$cookies.remove('sessionId');
+		    			$location.path('login');
+		    		});
+    			});
+		    },
+		    islogged:function(){
+		    	return $cookies.get('sessionId') ? true : false;
+		    }
+		}
+	});
+
+
 
 
 
